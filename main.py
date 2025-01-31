42e31add33ea4ae6a486508a54700ddf5d8484bc
Aleksei <u_alex90@mail.ru>
2025-01-31 18:18:03 +1000

    Crete project

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+from criptosem.api.views import router as criptosem_router
+
+
+app = FastAPI()
+
+app.include_router(criptosem_router)
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
