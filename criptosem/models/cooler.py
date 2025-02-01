f0e11ae6d5d08bcb1a8c33b8e43e7b1d37576365
Aleksei <u_alex90@mail.ru>
2025-02-01 23:22:27 +1000

    Create Cooler

--- criptosem/models/cooler.py
+++ criptosem/models/cooler.py
@@ -3,9 +3,14 @@ from sqlalchemy.orm import Mapped
 from core.models import Base
 
 
-class cooler(Base):
-    __tablename__ = 'products'  # Можно явно указать, если в базовом классе не использован declared_attr для именования таблиц
+class Сooler(Base):
+    """Модель "Охладитель"""
 
-    name: Mapped[str]           
-    description: Mapped[str]
-    price: Mapped[int]
+    name: Mapped[str]
+    state: Mapped[str]
+    ctrl_word: Mapped[int]
+    StateWord: Mapped[int]
+    FaultWord: Mapped[int]
+    MPState: Mapped[bool]
+    TemperCubeOn_Value: Mapped[float]
+    TimeStopAuto_Value: Mapped[float]
