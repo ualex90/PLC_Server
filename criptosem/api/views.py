a2117a3a3890a378d5c66c36af808462f665091b
Aleksei <u_alex90@mail.ru>
2025-02-01 23:23:29 +1000

    Create load data

--- criptosem/api/views.py
+++ criptosem/api/views.py
@@ -1,5 +1,16 @@
+import json
 from fastapi import APIRouter
 
+from core.models.base import Data
 
-router = APIRouter(prefix="/criptosem", tags="criptosem")
 
+router = APIRouter(prefix="/criptosem", tags=["criptosem"])
+
+# @router.post()
+# def load_data(data: Data):
+#     return Data.data
+
+@router.post("")
+def list_items(data: Data):
+    print(data.data["a"])
+    return data
